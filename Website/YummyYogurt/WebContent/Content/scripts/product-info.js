f593e9cfe4f22650e3d4f66d4db70076a0e8c059
DonColon <drrafshi@hotmail.de>
2018-05-29 01:46:07 +0200

    product-info.js refactored und Preis berechnet

--- Website/YummyYogurt/WebContent/Content/scripts/product-info.js
+++ Website/YummyYogurt/WebContent/Content/scripts/product-info.js
@@ -1,54 +1,64 @@
 $.ajax({
 	url: "http://localhost:8080/YummyYogurt/product-info.html",
     data: {id: 1},
-    type: "GET",
-    dataType: "json"
-})
-        	
-.done(function(json) {
-     $("h1").html(json.name);
-     $("li.breadcrumb-item.active").html(json.name);
-     
-     var categories = [];
-     var priceOfIngredient = [];
-     
-     $.each(json.recipe, function(index, ingredient) {
-    	 $("#recipe").append("<li>"+ingredient.name+"</li>");
-    	 categories.push(ingredient.category.name);
-    	 categories.push(ingredient.category.priceInCents);
-     });
-     
-     var price = 0;
-     for(var i = 0; i < priceOfIngredient.length; i++) {
-    	 price = price + priceOfIngredient[i];
-     }
-     price = price / 100;
-     $("#price").html("Preis pro Becher: " + price);
-     
-     categories = unique(categories);
-     categories = categories.filter(function(element) {
-    	 return element !== "Joghurtbasis" && element !== "Soßen";
-     });
-     
-     $.each(categories, function(index, category) {
-    	 $("#categories").append("<span>"+category+"</span>");
-    	 
-    	 switch(category) {
-    	 	case "Früchte":			$("#categories span").eq(index).addClass("badge badge-fruits"); 	break;
-    	 	case "Nüsse und Kerne":	$("#categories span").eq(index).addClass("badge badge-nuts");		break;
-    	 	case "Schoko":			$("#categories span").eq(index).addClass("badge badge-chocolate"); 	break;
-    	 	case "Süßigkeiten":		$("#categories span").eq(index).addClass("badge badge-sweets"); 	break;
-    	 }
-     });
-})
-        	
-.fail(function(xhr, status, errorThrown) {
-	alert("Sorry, there was a problem!");
-    console.log("Error: " + errorThrown);
-    console.log("Status: " + status );
-    console.log(xhr);
+    method: "GET",
+    dataType: "json",
+    
+    success: function(result) {
+    	editPage(result);
+    },
+    
+    error: function(xhr, status, errorThrown) {
+		handleError(xhr, status, errorThrown);
+	}
 });
 
+
+function editPage(productInfo) {
+    $("h1").html(productInfo.name);
+    $("li.breadcrumb-item.active").html(productInfo.name);
+    
+    createIngredientList(productInfo.recipe);
+    createCategorySection(productInfo.recipe);
+    calculatePrice(productInfo.recipe);
+}
+
+function createIngredientList(recipe) {
+	$.each(recipe, function(index, ingredient) {
+		$("#recipe").append("<li>" + ingredient.name + "</li>");
+	});
+}
+
+function createCategorySection(recipe) {
+	var categories = [];
+	$.each(recipe, function(index, ingredient) {
+		categories.push(ingredient.category.name);
+	});
+	
+	categories = unique(categories);
+	categories = categories.filter(function(element) {
+		return element !== "Joghurtbasis" && element !== "Soßen";
+	});
+	
+	$.each(categories, function(index, category) {
+		$("#categories").append("<span>"+category+"</span>");
+   	 
+		switch(category) {
+   	 		case "Früchte":			
+   	 			$("#categories span").eq(index).addClass("badge badge-fruits"); 	
+   	 			break;
+   	 		case "Nüsse und Kerne":	
+   	 			$("#categories span").eq(index).addClass("badge badge-nuts");		
+   	 			break;
+   	 		case "Schoko":			
+   	 			$("#categories span").eq(index).addClass("badge badge-chocolate"); 	
+   	 			break;
+   	 		case "Süßigkeiten":		
+   	 			$("#categories span").eq(index).addClass("badge badge-sweets"); 	
+   	 			break;
+   	 	}
+	});
+}
 
 function unique(array) {
     var resultSet = [];
@@ -58,4 +68,21 @@ function unique(array) {
         }
     }
     return resultSet;
+}
+
+function calculatePrice(recipe) {
+	var price = 0;
+	$.each(recipe, function(index, ingredient) {
+		price += ingredient.category.priceInCents;
+	});
+	price /= 100;
+	$("#price").html("Preis pro Becher: " + price.toFixed(2) + "€");
+}
+
+
+function handleError(xhr, status, errorThrown) {
+	alert("Sorry, there was a problem!");
+    console.log("Error: " + errorThrown);
+    console.log("Status: " + status );
+    console.log(xhr);
 }
